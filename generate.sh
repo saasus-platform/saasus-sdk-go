02dbd3c236fbd66cb807a16498fa8688259c991a
yutakahashi114 <yu.takahashi@anti-pattern.co.jp>
2023-11-15 15:39:34 +0900

    add mkdir to generate.sh

--- generate.sh
+++ generate.sh
@@ -8,5 +8,7 @@ fi
 PACKAGE_NAME=$1
 FILE_NAME=$2
 
+mkdir -p generated/${PACKAGE_NAME}api
+
 oapi-codegen -generate types -package "${PACKAGE_NAME}api" "$FILE_NAME" > "generated/${PACKAGE_NAME}api/types.gen.go"
 oapi-codegen -generate client -package "${PACKAGE_NAME}api" "$FILE_NAME" > "generated/${PACKAGE_NAME}api/client.gen.go"
